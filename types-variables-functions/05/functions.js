d099bdf4cc2a6bddcce70b247e56bd55aff1687e
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-29 17:45:02 -0300

    JavaScript: tipos, variáveis e funções - Aula 05

---
+++ types-variables-functions/05/functions.js
@@ -0,0 +1,14 @@
+function helloWorld(phrase, name) {
+    console.log(phrase, name) 
+};
+const hello = "Hello World";
+const myName = "John Doe";
+helloWorld(hello, myName);
+
+function sum(fNumber, sNumber) {
+    result = fNumber + sNumber;
+    return result;
+};
+const firstNumber = 3;
+const secondNumber = 5;
+console.log(sum(firstNumber, secondNumber));
